02a576bfceca8c70cac7d6260d3d4101b9ed54ac
Piotr Kapuściński <kapuscinskipiotr5@gmail.com>
2023-12-23 19:02:14 +0100

    deteled useless files and folders

--- app/(root)/cats/page.tsx
+++ app/(root)/cats/page.tsx
@@ -1,36 +1,9 @@
-'use client'
-import axios from 'axios';
-import {useState, useEffect} from 'react';
-import Image from 'next/image';
-
-const CatsPage: React.FC = () => {
-  const [cats, setCats] = useState([]);
-
-  useEffect(() => {
-    const fetchCats = async () => {
-      try {
-        const response = await axios.get('/api/cats');
-        setCats(response.data);
-      } catch (error) {
-        console.error('Błąd pobierania danych z API:', error);
-      }
-    };
-
-    fetchCats();
-  }, []);
-
+'use client';
+const CatsPage = () => {
+ 
   return (
     <div>
       <h1>Cats list product:</h1>
-      <ul>
-        {cats.map((cat) => (
-          <li key={cat._id}>
-            <Image src={cat.image} alt={cat.name} width={200} height={200} />
-            <p>{cat.name}</p>
-            <p>{cat.price}</p>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 };
--- app/middleware/database.ts
+++
@@ -1,7 +0,0 @@
-import {connectToDatabase} from '@/lib/database';
-const middleware = async (req, res, next) => {
-  req.db = await connectToDatabase();
-  return next();
-};
-
-export default middleware;
