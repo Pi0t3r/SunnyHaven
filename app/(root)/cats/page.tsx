0867181d6e4c89fefa1d1a509e6ffeecc588c22c
Piotr Kapuściński <kapuscinskipiotr5@gmail.com>
2023-12-24 16:13:53 +0100

    update next.config.ts

--- app/(root)/cats/page.tsx
+++ app/(root)/cats/page.tsx
@@ -1,18 +1,31 @@
 'use client';
 import {useEffect, useState} from 'react';
-interface CatData {
-  _id: number;
-  name: string;
-  price: number;
-}
-const CatsPage = () => {
-  const [cats, setCats] = useState<CatData[]>([]);
 
+import {CatFood} from '@/pages/models/catFood.model';
+import {
+  Box,
+  Text,
+  Heading,
+  Stack,
+  Card,
+  CardBody,
+  Divider,
+  CardFooter,
+  ButtonGroup,
+  Image,
+  UnorderedList,
+  Button,
+  ListItem,
+} from '@chakra-ui/react';
+import Link from 'next/link';
+const CatsPage = () => {
+  const [cats, setCats] = useState<CatFood[]>([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch('/api/foodCat');
-        const data: CatData[] = await response.json();
+        const data: CatFood[] = await response.json();
 
         if (Array.isArray(data)) {
           setCats(data);
@@ -21,6 +34,8 @@ const CatsPage = () => {
         }
       } catch (error) {
         console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -28,16 +43,55 @@ const CatsPage = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Cats list product:</h1>
-      <ul>
-        {cats.map((cat) => (
-          <li key={cat._id}>
-            {cat.name} - ${cat.price}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Box textAlign='center' padding={10}>
+      <Heading as='h2'>Karmy dla kotów</Heading>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <UnorderedList textAlign='center' listStyleType='none'>
+          {cats.map((cat) => (
+            <ListItem
+              key={cat._id.toString()}
+              width='fit-content'
+              margin='auto'
+            >
+                <Card maxW='sm'>
+                  <CardBody>
+                    <Image
+                      objectFit='cover'
+                      boxSize='200px'
+                      src={cat.imageUrl}
+                      alt={`${cat.name} image food`}
+                      borderRadius='lg'
+                      margin='auto'
+                    />
+                    <Stack mt='6' spacing='3'>
+                      <Heading size='md'>
+                        {cat.name} for {cat.ageCat}
+                      </Heading>
+                      <Text fontSize='sm'>{cat.desc}</Text>
+                      <Text color='blue.600' fontSize='2xl'>
+                        {cat.price} zł/szt
+                      </Text>
+                    </Stack>
+                  </CardBody>
+                  <Divider />
+                  <CardFooter>
+                    <ButtonGroup spacing='2'>
+                      <Button variant='solid' colorScheme='blue'>
+                        Buy now
+                      </Button>
+                      <Button variant='ghost' colorScheme='blue'>
+                        Add to cart
+                      </Button>
+                    </ButtonGroup>
+                  </CardFooter>
+                </Card>
+            </ListItem>
+          ))}
+        </UnorderedList>
+      )}
+    </Box>
   );
 };
 
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['unizoo.pl'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
