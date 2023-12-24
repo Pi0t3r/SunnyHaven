5b4d2ba382e05ed671bd16240a8e11427bbfeeaf
Piotr Kapuściński <kapuscinskipiotr5@gmail.com>
2023-12-24 16:13:05 +0100

    create file catFood.model.ts;added domains unizoo.pl for images;create a card for show cat food from MongoDB Atlas

---
+++ pages/models/catFood.model.ts
@@ -0,0 +1,69 @@
+import mongoose, {Schema, model, Document, ObjectId} from 'mongoose';
+
+const dbURL = process.env.MONGODB_URI;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(dbURL, options);
+mongoose.connection.on('error', (err) => {
+  console.error('Błąd połączenia z bazą danych.');
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Połączono z bazą danych');
+});
+export interface CatFood extends Document {
+  _id: ObjectId;
+  name: string;
+  price: number;
+  imageUrl: string;
+  weight: number;
+  taste: string;
+  desc: string;
+  calories: string;
+  ageCat: string;
+}
+
+const CatFoodSchema = new Schema<CatFood>({
+  _id: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  weight: {
+    type: Number,
+    required: true,
+  },
+  taste: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  calories: {
+    type: String,
+    required: true,
+  },
+  ageCat: {
+    type: String,
+    required: true,
+  },
+});
+
+const CatFood = model<CatFood>('CatFood', CatFoodSchema);
